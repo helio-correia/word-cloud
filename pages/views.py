b2919e9e4650518b734455c9a91dd14d61668864
Helio Correia <helio.as.correia@gmail.com>
2017-12-08 13:59:27 +0000

    Add test form for website word counter

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from .forms import WebsiteForm
+
 
 def home(request):
-    return render(request, 'pages/home.html')
+    words = []
+    if request.method == 'POST':
+        form = WebsiteForm(request.POST)
+        if form.is_valid():
+            words = ['some', 'random', 'words']
+
+    else:
+        form = WebsiteForm()
+
+    return render(request, 'pages/home.html', {'form': form, 'words': words})
